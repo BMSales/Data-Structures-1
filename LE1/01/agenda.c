ceb55243eb166ad1259d9185d7097b2a3026c99d
Bruno Sales <bruno.m.sales03@gmail.com>
2024-04-21 18:44:08 -0300

    Implemented user addition functionality

--- LE1/01/agenda.c
+++ LE1/01/agenda.c
@@ -3,13 +3,26 @@
 #include "struct.h"
 
 void Include(AGENDA **users, int* users_num_ptr){
+	char p_number[11];
+	(*users) = (AGENDA*)realloc(*users, ((*users_num_ptr) + 1) * sizeof(AGENDA));
+
+	printf("Name of the user: ");
+  	fgets((*users)[(*users_num_ptr)].name, 40, stdin);
+	printf("e-Mail of the user: ");
+	fgets((*users)[(*users_num_ptr)].email, 40, stdin);
+	printf("Phone number the user: ");
+	fgets(p_number, 11, stdin);
+	(*users)[(*users_num_ptr)].phone_num = strtol(p_number, NULL, 0);
+	printf("\n\n");
+
 	(*users_num_ptr) = (*users_num_ptr) + 1;
-	(*users) = (AGENDA*)realloc(*users, (*users_num_ptr) * sizeof(AGENDA));
 }
 
 void Display(AGENDA *users, int* users_num_ptr){
 	for(int i = 0; i < (*users_num_ptr); i++){
-		printf("name: %c\nemail: %c\nphone number: %d\n\n", users[i].name[0], users[i].email[0], users[i].phone_num);
+		puts(users[i].name);
+		puts(users[i].email);
+		printf("%d\n\n", users[i].phone_num);
 	}
 	printf("\n\n");
 }
